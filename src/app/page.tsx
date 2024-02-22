fae2d419d0f29e80441bd70391523d86633f3145
akio <124953279+akiomatic@users.noreply.github.com>
2024-02-21 21:49:17 -0800

    feat: add hero section w/ sticky navbar (#2)
    * feat: add header
    
    * feat: add placeholder my image
    
    * feat: add hero section
    
    * refactor: remove unused variables/imports
    
    * feat: render Header & Hero
    
    * feat: make Header sticky
    
    * refactor: use `nav` tag instead of `div`
    
    * refactor: use the same background color throughout page
    
    * feat: use the same default text color
    
    * refactor: remove unused parameter
    
    * fix: adjust width of navbar
    
    ---------
    
    Co-authored-by: akio <124953279+akiomatic108@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
+
 export default function Home() {
-	return <main>Welcome to my portfolio!</main>;
+	return (
+		<div className={"flex flex-col items-center text-[#E5ECF0] bg-[#0B1215]"}>
+			<Header />
+			<Hero />
+		</div>
+	);
 }
---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+const Hero = () => {
+	return (
+		<div
+			className={"h-screen w-full flex justify-center items-center space-x-20"}
+		>
+			<div className={"flex flex-col items-start w-[500px]"}>
+				<h1 className={"text-6xl"}>
+					Hello, I'm <span className={"text-lime-200 font-bold"}>Akio</span>!
+				</h1>
+				<h2 className={"text-3xl pt-2"}>A Front-End Developer</h2>
+				<p className={"text-lg pt-6 text-current/60"}>
+					I'm Akio Osawa, a passionate programmer since 2016,{" "}
+					<span
+						className={
+							"drop-shadow-3xl shadow-white font-semibold text-lime-200 cursor-pointer"
+						}
+					>
+						inspired by Minecraft
+					</span>
+					.
+				</p>
+				{/*<div className={"flex w-full my-5"}>*/}
+				{/*	<div>I'm</div>*/}
+				{/*	<div className={"text-[120px] leading-none ml-auto"}>Akio</div>*/}
+				{/*	<div className={"self-end pb-4"}>,</div>*/}
+				{/*</div>*/}
+				{/*<div className={"self-end"}>*/}
+				{/*	a <span className={"text-lime-300"}>Minecraft-inspired</span>*/}
+				{/*</div>*/}
+				{/*<div className={"self-end"}>Front-End Developer</div>*/}
+			</div>
+			<Image
+				src={"/images/placeholder-icon.webp"}
+				alt={"Placeholder icon"}
+				width={300}
+				height={300}
+				className={"rounded-full"}
+			/>
+		</div>
+	);
+};
+
+export default Hero;
