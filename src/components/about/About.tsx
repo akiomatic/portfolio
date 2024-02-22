55c490287ff72cae23b555048b7a5bc9109fdc05
akio <124953279+akiomatic108@users.noreply.github.com>
2024-02-22 09:10:00 -0800

    feat: highlight important words

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -10,20 +10,33 @@ const About = () => {
 			<div className={"mt-12 space-y-5 mx-10"}>
 				<div>
 					In 2015, I came across a game that significantly influenced my career
-					path: Minecraft. I didn't just play it for fun; it gave me a chance to
-					create and change a world or a server through coding. While designing
-					plugins and altering the game environment, I became deeply engrossed
-					in turning ideas into reality in Java. It was a huge eye-opener. The
-					ability to solve problems and build things in a digital world sparked
-					a real interest in programming for me.
+					path: <span className={"text-lime-300"}>Minecraft</span>. I didn't
+					just play it for fun; it gave me a chance to{" "}
+					<span className={"text-lime-300"}>create</span> and{" "}
+					<span className={"text-lime-300"}>change</span> a world or a server{" "}
+					<span className={"text-lime-300"}>through coding</span>. While
+					designing <span className={"text-lime-300"}>plugins</span> and
+					altering the game environment, I became deeply engrossed in{" "}
+					<span className={"text-lime-300"}>
+						turning ideas into reality in Java
+					</span>
+					. It was a huge eye-opener. The ability to solve problems and build
+					things in a digital world sparked a real interest in programming for
+					me.
 				</div>
 				<div>
-					Now, I find myself in Canada, pursing my dream to become a front-end
-					developer. This field, for me, is the perfect canvas to blend my
-					programming skills with my passion for creating intuitive, visually
-					compelling user experiences. It's here that I aim to innovate and
-					contribute to the digital landscape, inspired by the same curiosity
-					that Minecraft ignited in me years ago.
+					Now, I find myself in Canada, pursing my dream to become a{" "}
+					<span className={"text-lime-300"}>front-end developer</span>. This
+					field, for me, is the perfect canvas to blend my programming skills
+					with my passion for creating{" "}
+					<span className={"text-lime-300"}>intuitive</span>,{" "}
+					<span className={"text-lime-300"}>visually compelling</span> user
+					experiences. It's here that I aim to{" "}
+					<span className={"text-lime-300"}>innovate</span> and{" "}
+					<span className={"text-lime-300"}>contribute</span> to the digital
+					world, inspired by the same{" "}
+					<span className={"text-lime-300"}>curiosity</span> that Minecraft
+					ignited in me years ago.
 				</div>
 				<div>
 					By the way, in my free time, I still play Minecraft and enjoy Animal
