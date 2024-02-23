35fb4b347ace2196da46fa6096202ff264926a82
akio <124953279+akiomatic108@users.noreply.github.com>
2024-02-23 00:08:18 -0800

    feat: render Skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/about/About";
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import Skills from "@/components/skills/Skills";
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Header />
 			<Hero />
 			<About />
+			<Skills />
 		</div>
 	);
 }
