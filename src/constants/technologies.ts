ecd06033da57ace86f6be2f59d57253b3123dabd
akio <124953279+akiomatic108@users.noreply.github.com>
2024-02-24 23:54:33 -0800

    feat: add `Language` & `Tech` enums

---
+++ src/constants/technologies.ts
@@ -0,0 +1,16 @@
+export enum Language {
+	Java = "Java",
+	Dart = "Dart",
+	JavaScript = "JavaScript",
+	TypeScript = "TypeScript",
+}
+
+export enum Tech {
+	React = "React",
+	Next = "Next.js",
+	TailwindCSS = "Tailwind CSS",
+	Firebase = "Firebase",
+	Flutter = "Flutter",
+	ShadcnUI = "shadcn/ui",
+	Supabase = "supabase",
+}
