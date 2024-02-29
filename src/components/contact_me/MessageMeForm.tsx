b54a16051816ab1ff8ce444fe17bdc13df024424
akio <124953279+akiomatic108@users.noreply.github.com>
2024-02-29 10:30:01 -0800

    feat: show spinner while sending an email

--- src/components/contact_me/MessageMeForm.tsx
+++ src/components/contact_me/MessageMeForm.tsx
@@ -127,8 +127,26 @@ const MessageMeForm = ({ className, setOpen }: IMessageMeFormProps) => {
 						</FormItem>
 					)}
 				/>
-				<Button type="submit" className={"mt-4"}>
-					Send message
+				<Button
+					type="submit"
+					className={`mt-4 ${
+						isPending
+							? "bg-gray-400 hover:bg-gray-400 cursor-not-allowed"
+							: null
+					}`}
+				>
+					{isPending ? (
+						<div
+							className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+							role="status"
+						>
+							<span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+								Sending...
+							</span>
+						</div>
+					) : (
+						"Send message"
+					)}
 				</Button>
 			</form>
 		</Form>
