21a11c1b937f1293fb01c2c9365eceb102e49920
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-25 23:52:56 -0700

    style: change global font to Roboto_Mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { Toaster } from "@/components/shadcn_ui/toaster";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -24,7 +24,7 @@ export default function RootLayout({
 					defer
 				/>
 			</head>
-			<body className={inter.className}>
+			<body className={robotoMono.className}>
 				<main>{children}</main>
 				<Toaster />
 			</body>
