a5482cacd284b4b8d4f068071ee7c388e586fc74
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-26 21:23:29 -0700

    feat: modularize intro text to another file

---
+++ src/features/about/constants/text-data.ts
@@ -0,0 +1,6 @@
+export const INTRO_TEXT = `
+In 2015, I came across a game that significantly influenced my career path: **Minecraft**. I didn't just play it for fun; it gave me a chance to **create** and **customize* a world or a server **through coding**. While designing **plugins** and altering the game environment, I became deeply engrossed in **turning ideas into reality** in Java. It was a huge eye-opener. The ability to solve problems and build things in a digital world sparked a real interest in programming for me.
+
+Now, I find myself in Canada, pursing my dream to become a **front-end developer**. This field, for me, is the perfect canvas to blend my programming skills with my passion for creating **intuitive**, **visually compelling** user experiences. It's here that I aim to **innovate** and **contribute** to the digital world, inspired by the same **curiosity** that Minecraft ignited in me years ago.
+
+By the way, in my free time, I still play Minecraft and enjoy Animal Crossing, which helps me relax and fuel my creativity in unexpected ways.`;
