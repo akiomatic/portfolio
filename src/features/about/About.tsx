4bd889ecca1b3d0be3c9e7932ed5267b55c674c6
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-26 21:24:25 -0700

    feat: display intro text with react-markdown

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,3 +1,6 @@
+import { INTRO_TEXT } from "@/features/about/constants/text-data";
+import Markdown from "react-markdown";
+
 const About = () => {
 	return (
 		<div
@@ -7,42 +10,17 @@ const About = () => {
 			}
 		>
 			<div className={"text-4xl font-bold"}>About Me</div>
-			<div className={"mt-12 space-y-5 mx-10"}>
-				<div>
-					In 2015, I came across a game that significantly influenced my career
-					path: <span className={"text-lime-300"}>Minecraft</span>. I didn't
-					just play it for fun; it gave me a chance to{" "}
-					<span className={"text-lime-300"}>create</span> and{" "}
-					<span className={"text-lime-300"}>change</span> a world or a server{" "}
-					<span className={"text-lime-300"}>through coding</span>. While
-					designing <span className={"text-lime-300"}>plugins</span> and
-					altering the game environment, I became deeply engrossed in{" "}
-					<span className={"text-lime-300"}>
-						turning ideas into reality in Java
-					</span>
-					. It was a huge eye-opener. The ability to solve problems and build
-					things in a digital world sparked a real interest in programming for
-					me.
-				</div>
-				<div>
-					Now, I find myself in Canada, pursing my dream to become a{" "}
-					<span className={"text-lime-300"}>front-end developer</span>. This
-					field, for me, is the perfect canvas to blend my programming skills
-					with my passion for creating{" "}
-					<span className={"text-lime-300"}>intuitive</span>,{" "}
-					<span className={"text-lime-300"}>visually compelling</span> user
-					experiences. It's here that I aim to{" "}
-					<span className={"text-lime-300"}>innovate</span> and{" "}
-					<span className={"text-lime-300"}>contribute</span> to the digital
-					world, inspired by the same{" "}
-					<span className={"text-lime-300"}>curiosity</span> that Minecraft
-					ignited in me years ago.
-				</div>
-				<div>
-					By the way, in my free time, I still play Minecraft and enjoy Animal
-					Crossing, which helps me relax and fuel my creativity in unexpected
-					ways.
-				</div>
+			<div className={"text-lg mt-12 space-y-5 mx-10"}>
+				<Markdown
+					components={{
+						strong: (props) => {
+							const { node, ...rest } = props;
+							return <strong className={"text-lime-300"} {...rest} />;
+						},
+					}}
+				>
+					{INTRO_TEXT}
+				</Markdown>
 			</div>
 		</div>
 	);
