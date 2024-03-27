705c2afceeb119417bc889718bd8da9298259e31
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-27 15:38:40 -0700

    feat: revise self-introduction text

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -9,13 +9,25 @@ const About = () => {
 				"flex flex-col items-center justify-center w-screen xl:w-[1200px] py-24"
 			}
 		>
-			<div className={"text-4xl font-bold"}>About Me</div>
-			<div className={"text-lg mt-12 space-y-5 mx-10"}>
+			<div className={"text-4xl font-bold mb-12"}>About Me</div>
+			<div className={"text-lg space-y-3 mx-10"}>
 				<Markdown
 					components={{
-						strong: (props) => {
-							const { node, ...rest } = props;
+						em: ({ node, ...rest }) => {
+							return <em className={"not-italic text-amber-300"} {...rest} />;
+						},
+						strong: ({ node, ...rest }) => {
 							return <strong className={"text-lime-300"} {...rest} />;
+						},
+						h2: ({ node, ...rest }) => {
+							return (
+								<h2
+									className={
+										"text-2xl font-semibold [&:not(:first-child)]:pt-6"
+									}
+									{...rest}
+								/>
+							);
 						},
 					}}
 				>
--- src/features/about/constants/text-data.ts
+++ src/features/about/constants/text-data.ts
@@ -1,6 +1,8 @@
 export const INTRO_TEXT = `
-In 2015, I came across a game that significantly influenced my career path: **Minecraft**. I didn't just play it for fun; it gave me a chance to **create** and **customize* a world or a server **through coding**. While designing **plugins** and altering the game environment, I became deeply engrossed in **turning ideas into reality** in Java. It was a huge eye-opener. The ability to solve problems and build things in a digital world sparked a real interest in programming for me.
+## Who am I?
+Hello, again! I'm **_Akio Osawa_**, a passionate and dedicated **programmer** from Japan. I started coding back when I was 13, to solve problems that I encountered while playing the video game. Since then, I quickly fell in love with programming and have been on a journey to **master** it ever since.
 
-Now, I find myself in Canada, pursing my dream to become a **front-end developer**. This field, for me, is the perfect canvas to blend my programming skills with my passion for creating **intuitive**, **visually compelling** user experiences. It's here that I aim to **innovate** and **contribute** to the digital world, inspired by the same **curiosity** that Minecraft ignited in me years ago.
+## Minecraft and Me
+At the age of 12, I came across **Minecraft**, which significantly influenced my career path. I was initially just a player, but I soon discovered that I could become a **server owner** and **customize** the game environment however I wanted. From there, I spent countless hours learning how to **code** and **configure** my servers, which made me realize the excitement of **turning ideas into reality**. At the start, I was completely unfamiliar with programming and English, but I gradually grasped these by watching YouTube tutorials and mimicking the code I saw. This experience was my first step into the world of programming and has indeed shaped me into who I am today in **Canada**.
 
-By the way, in my free time, I still play Minecraft and enjoy Animal Crossing, which helps me relax and fuel my creativity in unexpected ways.`;
+By the way, in my free time, I still play Minecraft and sometimes enjoy Animal Crossing, which helps me relax and fuel my creativity in unexpected ways.`;
