338065fbdc3295429ba3791d9869241ba8b7146f
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-27 20:21:37 -0700

    style: add responsiveness

--- src/features/skills/components/SkillsContainer.tsx
+++ src/features/skills/components/SkillsContainer.tsx
@@ -1,5 +1,6 @@
 import Tooltip from "@/features/skills/components/Tooltip";
 import { Skill } from "@/features/skills/constants/skills-data";
+import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { SimpleIcon } from "simple-icons";
@@ -11,9 +12,9 @@ interface ISkillsContainerProps {
 
 const SkillsContainer = ({ children, title }: ISkillsContainerProps) => {
 	return (
-		<div className={"flex items-center"}>
-			<h2 className={"text-2xl font-bold md:w-[175px]"}>{title}</h2>
-			<div className={"flex"}>{children}</div>
+		<div className={cn("flex items-center gap-y-6", "flex-col md:flex-row")}>
+			<h2 className={"text-xl md:text-2xl font-bold md:w-[175px]"}>{title}</h2>
+			<div className={"grid grid-cols-5 gap-y-6 sm:flex"}>{children}</div>
 		</div>
 	);
 };
@@ -30,13 +31,18 @@ const Item = ({ iconData }: IItemProps) => {
 	if (isSkill(iconData)) {
 		return (
 			<Tooltip title={iconData.title}>
-				<div className={"mx-2.5"}>
+				<div
+					className={cn(
+						"relative mx-2.5",
+						"w-[30px] md:w-[35px] h-[30px] md:h-[35px]",
+					)}
+				>
 					<Image
 						src={iconData.url}
 						alt={iconData.title}
 						className={"object-contain"}
-						width={35}
-						height={35}
+						fill
+						sizes="(max-width: 768px) 30px, 35px"
 					/>
 				</div>
 			</Tooltip>
@@ -45,7 +51,12 @@ const Item = ({ iconData }: IItemProps) => {
 
 	return (
 		<Tooltip title={iconData.title}>
-			<div className={"w-[35px] h-[35px] fill-white mx-2.5"}>
+			<div
+				className={cn(
+					"relative mx-2.5 fill-white",
+					"w-[30px] md:w-[35px] h-[30px] md:h-[35px]",
+				)}
+			>
 				<svg
 					role={"img"}
 					viewBox={"0 0 24 24"}
