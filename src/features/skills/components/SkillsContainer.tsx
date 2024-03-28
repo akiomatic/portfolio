c19489d8793fe7d8453fee205620460736143af9
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-27 20:41:55 -0700

    refactor: optimize `Item` component

--- src/features/skills/components/SkillsContainer.tsx
+++ src/features/skills/components/SkillsContainer.tsx
@@ -23,21 +23,30 @@ interface IItemProps {
 	iconData: SimpleIcon | Skill;
 }
 
-const isSkill = (iconData: SimpleIcon | Skill): iconData is Skill => {
-	return !("path" in iconData);
+const isSimpleIcon = (iconData: SimpleIcon | Skill): iconData is SimpleIcon => {
+	return "path" in iconData;
 };
 
 const Item = ({ iconData }: IItemProps) => {
-	if (isSkill(iconData)) {
-		return (
-			<Tooltip title={iconData.title}>
-				<div
-					className={cn(
-						"relative mx-2.5 transition-all",
-						"hover:cursor-pointer hover:scale-125",
-						"w-[30px] md:w-[35px] h-[30px] md:h-[35px]",
-					)}
-				>
+	return (
+		<Tooltip title={iconData.title}>
+			<div
+				className={cn(
+					"relative mx-2.5 transition-all",
+					"hover:cursor-pointer hover:scale-125",
+					"w-[30px] md:w-[35px] h-[30px] md:h-[35px]",
+					isSimpleIcon(iconData) && "fill-white",
+				)}
+			>
+				{isSimpleIcon(iconData) ? (
+					<svg
+						role={"img"}
+						viewBox={"0 0 24 24"}
+						xmlns={"http://www.w3.org/2000/svg"}
+					>
+						<path d={iconData.path} />
+					</svg>
+				) : (
 					<Image
 						src={iconData.url}
 						alt={iconData.title}
@@ -45,27 +54,7 @@ const Item = ({ iconData }: IItemProps) => {
 						fill
 						sizes="(max-width: 768px) 30px, 35px"
 					/>
-				</div>
-			</Tooltip>
-		);
-	}
-
-	return (
-		<Tooltip title={iconData.title}>
-			<div
-				className={cn(
-					"relative mx-2.5 fill-white transition-all",
-					"hover:cursor-pointer hover:scale-125",
-					"w-[30px] md:w-[35px] h-[30px] md:h-[35px]",
 				)}
-			>
-				<svg
-					role={"img"}
-					viewBox={"0 0 24 24"}
-					xmlns={"http://www.w3.org/2000/svg"}
-				>
-					<path d={iconData.path} />
-				</svg>
 			</div>
 		</Tooltip>
 	);
