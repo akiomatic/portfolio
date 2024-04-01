53ec77550773c656dbba43e0377d4f10a8f6615c
akio <124953279+akiomatic108@users.noreply.github.com>
2024-03-31 20:48:08 -0700

    style: adjust the button to open switcher

--- src/features/navbar/components/ThemeToggle.tsx
+++ src/features/navbar/components/ThemeToggle.tsx
@@ -7,6 +7,7 @@ import {
 	DropdownMenuItem,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 
@@ -16,13 +17,20 @@ export const ThemeToggle = () => {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<Button variant="outline" size="icon">
-					<SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+				<Button
+					size={"icon"}
+					className={cn(
+						"h-[30px] w-[30px] transition-all ring-transparent focus-visible:ring-transparent",
+						"hover:bg-white hover:bg-opacity-20 sm:hover:bg-opacity-30",
+						"bg-transparent sm:bg-white sm:bg-opacity-20",
+					)}
+				>
+					<SunIcon className="size-[15px] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					<MoonIcon className="absolute size-[15px] text-white rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 					<span className="sr-only">Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
-			<DropdownMenuContent align="end">
+			<DropdownMenuContent className={"mt-1"} align="end">
 				<DropdownMenuItem onClick={() => setTheme("light")}>
 					Light
 				</DropdownMenuItem>
