fb284b1ae947fb438be916102aaae7948841205c
akio <124953279+akiomatic@users.noreply.github.com>
2024-04-01 19:35:54 -0700

    style: enhance `WorkCard` button appearance for empty `content` (#24)
    * refactor: simplify props passing
    
    * feat: add `content` property to `Work`
    
    * feat: add style and text when `content` is empty
    
    ---------
    
    Co-authored-by: akio <124953279+akiomatic108@users.noreply.github.com>

--- src/features/works/components/WorkCard.tsx
+++ src/features/works/components/WorkCard.tsx
@@ -1,6 +1,5 @@
 import SVGIcon from "@/components/icons/SVGIcon";
 import { Button } from "@/components/ui/button";
-import { Icon } from "@/constants/enums";
 import { Work } from "@/features/works/constants/works-data";
 import { cn, isSimpleIcon } from "@/lib/utils";
 import {
@@ -12,137 +11,125 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import Markdown from "react-markdown";
-import { SimpleIcon } from "simple-icons";
 
-interface IImageSectionProps {
-	image: string;
-	team?: boolean;
-}
-const ImageSection = ({ image, team }: IImageSectionProps) => (
-	<div className="relative w-full h-full flex justify-center items-center">
-		<Image
-			key={image}
-			src={image}
-			alt="Sample image"
-			fill
-			sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-			className="object-cover rounded-lg"
-		/>
-		{team && (
-			<p className="absolute top-3 right-3 rounded-full bg-gray-200/70 dark:bg-black/70 text-sm px-3 py-1">
-				Team
-			</p>
-		)}
-	</div>
-);
-
-interface ITechStackProps {
-	techStack: Array<SimpleIcon | Icon>;
-}
-const TechStack = ({ techStack }: ITechStackProps) => (
-	<div className="flex gap-x-4 mt-4">
-		{techStack.map((tech) => (
-			<SVGIcon
-				key={isSimpleIcon(tech) ? tech.slug : tech}
-				icon={tech}
-				className="w-[20px] h-[20px] fill-black dark:fill-white"
-			/>
-		))}
-	</div>
-);
-
-interface IOverviewProps {
-	date: string | number;
-	title: string;
-	overview: string;
-}
-
-const Overview = ({ date, title, overview }: IOverviewProps) => (
-	<>
-		<p className="text-sm mt-4">{date}</p>
-		<h2 className="text-xl font-semibold">{title}</h2>
-		<div className="flex-1 mt-2 overflow-hidden">
-			<Markdown
-				components={{
-					em: ({ node, ...rest }) => {
-						return (
-							<em
-								className={"not-italic text-violet-900 dark:text-amber-300"}
-								{...rest}
-							/>
-						);
-					},
-					strong: ({ node, ...rest }) => {
-						return (
-							<strong
-								className={"font-normal text-purple-700 dark:text-lime-300"}
-								{...rest}
-							/>
-						);
-					},
-				}}
-			>
-				{overview}
-			</Markdown>
-		</div>
-	</>
-);
-
-interface IActionLinksProps {
-	id: string;
-	github?: string;
-	website?: string;
-	video?: string;
-}
-const ActionLinks = ({ id, github, website, video }: IActionLinksProps) => (
-	<div className="flex justify-between items-center mt-4">
-		<div className="flex gap-x-2">
-			{github && (
-				<Button variant={"secondary"} size={"icon"} asChild>
-					<Link href={github} target="_blank" rel="noopener noreferrer">
-						<GitHubLogoIcon className="w-5 h-5" />
-					</Link>
-				</Button>
-			)}
-			{website && (
-				<Button variant={"secondary"} size={"icon"} asChild>
-					<Link href={website} target="_blank" rel="noopener noreferrer">
-						<ExternalLinkIcon className="w-5 h-5" />
-					</Link>
-				</Button>
-			)}
-			{video && (
-				<Button variant={"secondary"} size={"icon"} asChild>
-					<Link href={video} target="_blank" rel="noopener noreferrer">
-						<VideoIcon className="w-5 h-5" />
-					</Link>
-				</Button>
-			)}
-		</div>
-		<Button asChild>
-			<p>Learn more</p>
-		</Button>
-	</div>
-);
-
-interface IWorkCardProps {
+interface IGlobalProps {
 	work: Work;
 }
 
-const WorkCard = ({ work }: IWorkCardProps) => {
-	const {
-		id,
-		date,
-		team,
-		title,
-		overview,
-		image,
-		techStack,
-		github,
-		website,
-		video,
-	} = work;
+const ImageSection = ({ work }: IGlobalProps) => {
+	const { image, team } = work;
 
+	return (
+		<div className="relative w-full h-full flex justify-center items-center">
+			<Image
+				key={image}
+				src={image}
+				alt="Sample image"
+				fill
+				sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+				className="object-cover rounded-lg"
+			/>
+			{team && (
+				<p className="absolute top-3 right-3 rounded-full bg-gray-200/70 dark:bg-black/70 text-sm px-3 py-1">
+					Team
+				</p>
+			)}
+		</div>
+	);
+};
+
+const TechStack = ({ work }: IGlobalProps) => {
+	const { techStack } = work;
+
+	return (
+		<div className="flex gap-x-4 mt-4">
+			{techStack.map((tech) => (
+				<SVGIcon
+					key={isSimpleIcon(tech) ? tech.slug : tech}
+					icon={tech}
+					className="w-[20px] h-[20px] fill-black dark:fill-white"
+				/>
+			))}
+		</div>
+	);
+};
+
+const Overview = ({ work }: IGlobalProps) => {
+	const { date, title, overview } = work;
+
+	return (
+		<>
+			<p className="text-sm mt-4">{date}</p>
+			<h2 className="text-xl font-semibold">{title}</h2>
+			<div className="flex-1 mt-2 overflow-hidden">
+				<Markdown
+					components={{
+						em: ({ node, ...rest }) => {
+							return (
+								<em
+									className={"not-italic text-violet-900 dark:text-amber-300"}
+									{...rest}
+								/>
+							);
+						},
+						strong: ({ node, ...rest }) => {
+							return (
+								<strong
+									className={"font-normal text-purple-700 dark:text-lime-300"}
+									{...rest}
+								/>
+							);
+						},
+					}}
+				>
+					{overview}
+				</Markdown>
+			</div>
+		</>
+	);
+};
+
+const ActionLinks = ({ work }: IGlobalProps) => {
+	const { github, website, video, content } = work;
+
+	return (
+		<div className="flex justify-between items-center mt-4">
+			<div className="flex gap-x-2">
+				{github && (
+					<Button variant={"secondary"} size={"icon"} asChild>
+						<Link href={github} target="_blank" rel="noopener noreferrer">
+							<GitHubLogoIcon className="w-5 h-5" />
+						</Link>
+					</Button>
+				)}
+				{website && (
+					<Button variant={"secondary"} size={"icon"} asChild>
+						<Link href={website} target="_blank" rel="noopener noreferrer">
+							<ExternalLinkIcon className="w-5 h-5" />
+						</Link>
+					</Button>
+				)}
+				{video && (
+					<Button variant={"secondary"} size={"icon"} asChild>
+						<Link href={video} target="_blank" rel="noopener noreferrer">
+							<VideoIcon className="w-5 h-5" />
+						</Link>
+					</Button>
+				)}
+			</div>
+			<Button
+				className={cn(
+					"disabled:pointer-events-auto disabled:cursor-not-allowed",
+				)}
+				disabled={!content}
+			>
+				{!content ? "Coming soon..." : "Learn more"}
+			</Button>
+		</div>
+	);
+};
+
+const WorkCard = ({ work }: IGlobalProps) => {
 	return (
 		<div
 			className={cn(
@@ -150,11 +137,11 @@ const WorkCard = ({ work }: IWorkCardProps) => {
 				"hover:scale-105",
 			)}
 		>
-			<ImageSection image={image} team={team} />
+			<ImageSection work={work} />
 			<div className="flex flex-col row-span-2">
-				<TechStack techStack={techStack} />
-				<Overview date={date} title={title} overview={overview} />
-				<ActionLinks id={id} github={github} website={website} video={video} />
+				<TechStack work={work} />
+				<Overview work={work} />
+				<ActionLinks work={work} />
 			</div>
 		</div>
 	);
